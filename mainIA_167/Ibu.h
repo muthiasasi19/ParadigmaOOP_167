caa082346a0b6197738e2636a3927be3f5c6dbb2
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-05-22 03:17:00 -0700

    class ibu.h

---
+++ mainIA_167/Ibu.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef IBU_H
+#define IBU_H
+#include <iostream>
+#include "anak.h"
+#include <vector>
+using namespace std;
+
+class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "ibu \"" << nama << "\" ada\n";
+	}
+
+	~ibu() {
+		cout<< "ibu \"" << nama << "\" tidak ada\n";
+	}
+	void tambahAnak(anak*);
+	void cetakAnak();
+};
+
+
