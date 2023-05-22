e69002a80f881c66b892ca07329bacd82e9f768e
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-05-22 05:51:48 -0700

    int main Asosiasi

---
+++ Memberinlist_167/Memberinlist_167.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
+
+public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { //Member Initialization List
+        //Definisi
+    }
